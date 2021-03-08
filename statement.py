0c9fb258b6cd5c799a5a19d309a12f8b5fc425b1
Carla Davis <carlabonesdavis@gmail.com>
2021-03-07 22:51:54 -0500

    second committ

---
+++ statement.py
@@ -0,0 +1,9 @@
+# Exercise 1: The if statement
+userReply = input("Do you need to ship a package? (Type yes or no) ")
+userReply = isyes(userReply)
+if userReply == True:
+    print("We can help you ship that package!")
+    print(We can help you ship tha package today")
+    print("We can help you ship that package today!")
+    abc
+    
